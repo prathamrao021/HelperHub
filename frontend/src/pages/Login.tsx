d0492c8264001127877e66791e0f746354c1b55f
akash-2303 <akubala23@gmail.com>
2025-02-04 10:13:25 -0500

    Feat: Login Page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,86 @@
-const Login = () => {
-    return (
-    <div>Login Page</div>
-)
-}
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Typography, Container, Box, Avatar, Link } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box mt={5} sx={{ textAlign: 'center' }}>
+        {/* Lock Icon */}
+        <Avatar sx={{ mx: 'auto', bgcolor: 'secondary.main', mb: 2 }}>
+          <LockOutlinedIcon />
+        </Avatar>
+
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {/* Email Field */}
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                type="email"
+                fullWidth
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+
+            {/* Password Field */}
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                type="password"
+                fullWidth
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+
+            {/* Submit Button */}
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+
+        {/* Forgot Password Link */}
+        <Box mt={2}>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Box>
+
+        {/* Sign Up Link */}
+        <Box mt={2}>
+          <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
