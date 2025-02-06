75edb06f13d2949ce510878c11c6f1c9bd90cbd4
akash-2303 <akubala23@gmail.com>
2025-02-06 11:27:25 -0500

    Feat: Implemented NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,29 @@
-const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+import * as React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom'; 
 
-export default Navbar
+export default function Navbar() {
+  return (
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My App
+        </Typography>
+        <Box sx={{ display: 'flex' }}>
+          <Button color="inherit" component={Link} to="/home">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit" component={Link} to="/contact">
+            Contact
+          </Button>
+          <Button color="inherit" component={Link} to="/Profile">
+            Profile
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
